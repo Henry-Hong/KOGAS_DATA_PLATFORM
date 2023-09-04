7bab7a60e2b07456232ea12f0397127341611563
Henry-Hong <hhr8540@naver.com>
2023-09-04 21:06:50 +0900

    Chore : 상태관리라이브러리 pinia 설치

---
+++ src/components/LittleHelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>Hello, This is Little {{ name }} made Hello World!</div>
+</template>
+
+<script>
+export default {
+	name: "LittleHelloWorld",
+	props: {
+		name: String,
+	},
+};
+</script>
+
+<style scoped>
+div {
+	color: red;
+}
+</style>
