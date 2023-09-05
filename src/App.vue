0c61ad23e6d01ae6679bfca584642521c9af8111
Henry-Hong <hhr8540@naver.com>
2023-09-05 21:58:28 +0900

    Feat : 불필요한 푸터 헤더 삭제

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,12 @@
 <!-- normal 25 65 148 -->
 
 <template>
-	<v-layout ref="app" class="rounded rounded-md">
-		<v-app-bar name="app-bar">
-			<child v-slot="{ print }">
-				<v-btn class="mx-auto" @click="print('app-bar')">Get data</v-btn>
-			</child>
-		</v-app-bar>
-
+	<v-layout ref="app" class="rounded">
 		<v-main class="flex w-full items-center flex-col">
 			<img
 				src="./assets/Kogas_Icon.svg"
 				alt="KogasLogo"
-				class="h-[100px] mt-12 scale-90"
+				class="h-[100px] mt-24 scale-90"
 			/>
 			<div
 				class="flex flex-col w-[35%] gap-5 bg-white mt-14 justify-center rounded-tl-2xl p-10"
@@ -29,30 +23,10 @@
 				<button class="hover:underline cursor-pointer">회원가입</button>
 			</div>
 		</v-main>
-
-		<v-footer app name="footer">
-			<v-btn class="mx-auto" variant="text" @click="print('footer')">
-				Get data
-			</v-btn>
-		</v-footer>
 	</v-layout>
 </template>
 <script>
-import { useLayout } from "vuetify";
-
-const Child = {
-	setup(props, ctx) {
-		const { getLayoutItem } = useLayout();
-
-		function print(key) {
-			alert(JSON.stringify(getLayoutItem(key), null, 2));
-		}
-
-		return () => ctx.slots.default({ print });
-	},
-};
-
 export default {
-	components: { Child },
+	components: {},
 };
 </script>
