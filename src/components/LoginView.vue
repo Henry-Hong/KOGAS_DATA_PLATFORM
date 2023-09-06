8cfdf8d6af861284cea45588fc6b66d548b6210d
Henry-Hong <hhr8540@naver.com>
2023-09-06 14:17:59 +0900

    Style : 글씨수정

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,13 +1,13 @@
 <template>
 	<v-layout ref="app" class="rounded">
-		<v-main class="flex w-full items-center flex-col">
+		<v-main class="flex w-full h-screen items-center justify-center flex-col">
 			<img
 				src="../assets/Kogas_Icon.svg"
 				alt="KogasLogo"
-				class="h-[100px] mt-24 scale-90"
+				class="h-[100px] scale-90"
 			/>
 			<div
-				class="flex flex-col w-[35%] gap-5 bg-white mt-14 justify-center rounded-tl-2xl p-10"
+				class="flex flex-col w-[600px] gap-5 bg-white mt-14 justify-center rounded-tl-2xl p-10"
 			>
 				<h1 class="text-2xl font-bold">로그인</h1>
 				<input class="p-3 border" placeholder="아이디" />
