9fdfa35304823db08c3491692f0c4286bf8cc540
Henry-Hong <hhr8540@naver.com>
2023-09-06 09:27:27 +0900

    Feat : 메인에 푸터추가

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -14,28 +14,45 @@
 
 				<ul class="w-full flex justify-around p-2 text-white">
 					<li
-						class="w-52 h-52 flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-blue-600 hover:bg-blue-600/70 active:bg-blue-600/50 cursor-pointer"
+						class="w-1/6 aspect-square flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-blue-600 hover:bg-blue-600/70 active:bg-blue-600/50 cursor-pointer"
 					>
 						Miner Report
 					</li>
 					<li
-						class="w-52 h-52 flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-sky-500 hover:bg-sky-500/70 active:bg-sky-500/50 cursor-pointer"
+						class="w-1/6 aspect-square flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-sky-500 hover:bg-sky-500/70 active:bg-sky-500/50 cursor-pointer"
 					>
 						New Workspace
 					</li>
 					<li
-						class="w-52 h-52 flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-indigo-600 hover:bg-indigo-600/70 active:bg-indigo-600/50 cursor-pointer"
+						class="w-1/6 aspect-square flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-indigo-600 hover:bg-indigo-600/70 active:bg-indigo-600/50 cursor-pointer"
 					>
 						My Workspace
 					</li>
 					<li
-						class="w-52 h-52 flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-purple-500 hover:bg-purple-500/70 active:bg-purple-500/50 cursor-pointer"
+						class="w-1/6 aspect-square flex justify-center items-center font-bold rounded-full text-center transition-all p-6 bg-purple-500 hover:bg-purple-500/70 active:bg-purple-500/50 cursor-pointer"
 					>
 						Dash Board
 					</li>
 				</ul>
 			</div>
 		</v-main>
+		<footer class="w-full bottom-0 h-auto bg-[rgb(224,224,224)] absolute p-4">
+			<div class="flex items-center gap-10">
+				<img class="" src="../assets/Footer_Icon.png" />
+				<div class="flex flex-col">
+					<div class="flex gap-5">
+						<span>41062 대구광역시 동구 첨단로 120</span>
+						<span>|</span>
+						<span>대표전화: (053) 670-0114</span>
+						<span>|</span>
+						<span>팩스번호: (053) 670-0117</span>
+						<span>|</span>
+						<span>불공정 개래행위 및 갑질행위 신고센터: (053) 670-0112</span>
+					</div>
+					<p>COPYRIGHT Ⓒ2022 KOREA GAS CORPORATION ALL RIGHTS RESERVED</p>
+				</div>
+			</div>
+		</footer>
 	</v-layout>
 </template>
 
